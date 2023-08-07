ed7bb9ebc0bd9061295637cc289e30f0636ca95a
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-08-07 16:59:09 +0800

    Added interface

--- forge/src/interfaces/IERC20.sol
+++ forge/src/interfaces/IERC20.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 interface IERC20 {
+    function symbol() external view returns (string memory);
+
     function approve(address, uint256) external;
 
     function balanceOf(address) external returns (uint256);
---
+++ forge/src/interfaces/IUniswapV3Pool.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+interface IUniswapV3Pool {
+    struct CallbackData {
+        address token0;
+        address token1;
+        address payer;
+    }
+
+ 
+}
+
+
+
