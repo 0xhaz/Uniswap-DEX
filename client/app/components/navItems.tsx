70bc03c155cfc478509f0774501ec99778e2f0f0
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-10-11 05:23:44 +0800

    fixed navItems

--- client/app/components/navItems.tsx
+++ client/app/components/navItems.tsx
@@ -3,69 +3,37 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 const NavItems = () => {
-  const SWAP = "Swap";
-  const POOL = "Pool";
-  const STAKE = "Stake";
-  const LENDING = "Lending";
-  const FAUCET = "Faucet";
+  const navItems = ["Swap", "Pool", "Stake", "Lending", "Faucet"];
 
-  const [navItems, setNavItems] = useState(SWAP);
+  const [selectedItem, setSelectedItem] = useState("Swap");
 
   function getNavIconClassName(name: string) {
-    let className =
-      "p-1 px-4 cursor-pointer border-[4px] border-transparent flex items-center ";
-    className +=
-      name === navItems
-        ? " bg-zinc-800 w-[100px] justify-center border-zinc-900 rounded-full"
-        : "";
-    return className;
+    const baseClassName =
+      "p-1 px-4 cursor-pointer border-[4px] border-transparent flex items-center";
+    return `${baseClassName} ${
+      selectedItem === name
+        ? "border-zinc-800 w-[100px] justify-center border-zinc-900 rounded-full"
+        : ""
+    }`;
   }
 
+  const handleItemClick = (name: string) => {
+    setSelectedItem(name);
+  };
+
   return (
-    <>
-      <div className="bg-zinc-900 h-[50px] lg:w-[600px] flex items-center justify-between rounded-full mx-auto ">
-        <Link href="/">
+    <div className="bg-zinc-900 h-[50px] lg:w-[600px] flex items-center justify-between rounded-full mx-auto">
+      {navItems.map(item => (
+        <Link key={item} href={`/${item.toLowerCase()}`}>
           <div
-            className={getNavIconClassName(SWAP)}
-            onClick={() => setNavItems(SWAP)}
+            className={getNavIconClassName(item)}
+            onClick={() => handleItemClick(item)}
           >
-            {SWAP}
+            {item}
           </div>
         </Link>
-        <Link href="/pool">
-          <div
-            className={getNavIconClassName(POOL)}
-            onClick={() => setNavItems(POOL)}
-          >
-            {POOL}
-          </div>
-        </Link>
-        <Link href="/stake">
-          <div
-            className={getNavIconClassName(STAKE)}
-            onClick={() => setNavItems(STAKE)}
-          >
-            {STAKE}
-          </div>
-        </Link>
-        <Link href="/lending">
-          <div
-            className={getNavIconClassName(LENDING)}
-            onClick={() => setNavItems(LENDING)}
-          >
-            {LENDING}
-          </div>
-        </Link>
-        <Link href="/faucet">
-          <div
-            className={getNavIconClassName(FAUCET)}
-            onClick={() => setNavItems(FAUCET)}
-          >
-            {FAUCET}
-          </div>
-        </Link>
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
