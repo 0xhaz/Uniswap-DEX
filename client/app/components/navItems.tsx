956c1168758133b72fb33558a575d88c19458543
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-10-11 05:29:02 +0800

    fixed navItems

--- client/app/components/navItems.tsx
+++ client/app/components/navItems.tsx
@@ -1,21 +1,19 @@
-"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 
 const NavItems = () => {
   const navItems = ["Swap", "Pool", "Stake", "Lending", "Faucet"];
-
   const [selectedItem, setSelectedItem] = useState("Swap");
 
-  function getNavIconClassName(name: string) {
+  const getNavIconClassName = (name: string) => {
     const baseClassName =
       "p-1 px-4 cursor-pointer border-[4px] border-transparent flex items-center";
     return `${baseClassName} ${
       selectedItem === name
-        ? "border-zinc-800 w-[100px] justify-center border-zinc-900 rounded-full"
+        ? "bg-zinc-800 w-[100px] justify-center border-zinc-900 rounded-full"
         : ""
     }`;
-  }
+  };
 
   const handleItemClick = (name: string) => {
     setSelectedItem(name);
@@ -24,7 +22,10 @@ const NavItems = () => {
   return (
     <div className="bg-zinc-900 h-[50px] lg:w-[600px] flex items-center justify-between rounded-full mx-auto">
       {navItems.map(item => (
-        <Link key={item} href={`/${item.toLowerCase()}`}>
+        <Link
+          key={item}
+          href={item === "Swap" ? "/" : `/${item.toLowerCase()}`}
+        >
           <div
             className={getNavIconClassName(item)}
             onClick={() => handleItemClick(item)}
