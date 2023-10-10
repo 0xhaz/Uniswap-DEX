bbbfec49fb6729628681e1e0b6e172699a68c07c
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-10-11 05:57:54 +0800

    fixed tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
